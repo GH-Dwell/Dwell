25546539ecf5661feaa8b79e5bbedb57d1e00c48
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-08-20 16:59:21 +1000

    init front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select'
+import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import { Map, Marker } from "pigeon-maps"
 
 function App() {
+  const stateOptions = [
+    { value: "nsw", label: "NSW" },
+    { value: "qld", label: "QLD" },
+    { value: "wa", label: "WA" },
+    { value: "sa", label: "SA" },
+    { value: "vic", label: "VIC" },
+  ];
+
+  const cityOptions = [
+    { value: "sydney", label: "Sydney"},
+    { value: "newcastle", label: "Newcastle"},
+    { value: "orange", label: "Orange"},
+    { value: "tamworth", label: "Tamworth"},
+    { value: "wagga", label: "Wagga Wagga"},
+  ];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div className='app-header'>
+          <h1>Dwell</h1>
+        </div>
+        <div className='app-map'>
+          <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+            <Marker width={50} anchor={[50.879, 4.6997]} />
+          </Map>
+        </div>
+        <div className='app-body'>
+          <Select
+            options={stateOptions}
+            placeholder="Select a State"
+          />
+          <p></p>
+          <Select
+            options={cityOptions}
+            placeholder="Select a City"
+          />
+          <p></p>
+          <Switch
+            // checked={checked}
+            // onChange={handleChange}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
+          <p></p>
+          <Button variant="contained">Search</Button>
+        </div>
+      </div>
+  </div>
   );
 }
 
