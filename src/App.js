dd8c21062a70665aeefffd256b12f046604ba1d6
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-08-21 12:11:10 +1000

    dialog box on geo click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import React, { PureComponent, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Select from 'react-select'
 import Button from '@mui/material/Button';
 import Switch from '@mui/material/Switch';
-import { Map, Marker, GeoJsonLoader } from "pigeon-maps"
-// import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css';
+import { Map, Marker, GeoJsonLoader, Overlay } from "pigeon-maps"
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
 import Sa2API from './api/sa2.js'; 
 
@@ -36,18 +35,26 @@ function App() {
     coords: [-33.8688, 151.2093], 
     cityValue: "Sydney", 
     zoom: 11, 
-    mark1: [0,0], 
+    mark: [0,0], 
     rent: false, 
   }); 
 
   const [coor, setCoor] = useState([-33.8688, 151.2093]);
   const [zoom, setZoom] = useState(5);
-  const [mark1, setMark1] = useState([0, 0]);
-  const [rent, setRent] = useState(false);
+  const [mark, setMark] = useState([0, 0]);
   const [geoJsonLinkG, setGeoJsonLinkG] = useState("");
   const [geoJsonLinkR, setGeoJsonLinkR] = useState("");
   const [geoJsonLinkO, setGeoJsonLinkO] = useState("");
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     Sa2API.getCities().then((res) => {
@@ -58,7 +65,7 @@ function App() {
   const setAll = () => {
     setCoor(formState.coords);
     setZoom(formState.zoom);
-    setMark1(formState.mark1);
+    setMark(formState.mark);
     // setGeoJsonLink(["https://raw.githubusercontent.com/codeforgermany/click_that_hood/main/public/data/sydney.geojson"]);
     setGeoJsonLinkG(["https://raw.githubusercontent.com/GH-Dwell/Dwell/master/src/mock/sydney_green.geojson"]);
     setGeoJsonLinkR(["https://raw.githubusercontent.com/GH-Dwell/Dwell/master/src/mock/sydney_red.geojson"]);
@@ -72,25 +79,11 @@ function App() {
           <h1>Dwell</h1>
         </div>
         <div className='app-map'>
-        {/* <MapContainer 
-          // style={{height: '50vh', width: '50wh'}}
-          center={coor}
-          zoom={zoom}
-          scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={mark1}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer> */}
           <Map height={300}
             defaultCenter={coor} defaultZoom={4} zoom={zoom} center={coor}>
             <GeoJsonLoader
               link={geoJsonLinkG}
+              onClick={handleClickOpen}
               styleCallback={(feature, hover) =>
                 hover
                   ? { fill: '#5BD82299', strokeWidth: '2'}
@@ -99,6 +92,7 @@ function App() {
             />
             <GeoJsonLoader
               link={geoJsonLinkR}
+              onClick={handleClickOpen}
               styleCallback={(feature, hover) =>
                 hover
                   ? { fill: '#AE432C99', strokeWidth: '2'}
@@ -107,14 +101,36 @@ function App() {
             />
             <GeoJsonLoader
               link={geoJsonLinkO}
+              onClick={handleClickOpen}
               styleCallback={(feature, hover) =>
                 hover
                   ? { fill: '#a79c1c99', strokeWidth: '2'}
                   : { fill: '#fff15b99', strokeWidth: '1'}
               }
             />
-            <Marker width={50} anchor={mark1} />
+            <Marker width={50} anchor={mark} />
           </Map>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Location Name Here"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Stats/Info here.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleClose} color="primary" autoFocus>
+                Okay
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
         <div className='app-body'>
           <p></p>
@@ -148,35 +164,5 @@ function App() {
   </div>
   );
 }
-
-// class App extends PureComponent {
-//   render() {
-//     // create an array with marker components
-//     // const LeafletMarkers = markers.map((marker) => (
-//     const LeafletMarkers = () => (
-//       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-//       <TileLayer
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-//       <Marker position={[51.505, -0.09]}>
-//         <Popup>
-//           A pretty CSS3 popup. <br /> Easily customizable.
-//         </Popup>
-//       </Marker>
-//     </MapContainer>
-//     );
-
-//     const test = () => (
-//       <p>Test</p>
-//     )
-
-//     return (
-//       <div className="map">
-//           {LeafletMarkers}
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
